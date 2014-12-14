54aa2653144d564bf927a94877a524ecff89479f
Eniz Gülek <enizgulek@gmail.com>
2014-12-14 14:15:28 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-react-creditcard-input
-======================
+# React Credit Card Input
+
+React component for credit card input.
+
+## Installation
+
+React Input Calendar is available as an [npm package](https://www.npmjs.org/package/react-input-calendar).
+```sh
+npm install react-input-creditcard
+```
+
+Use [browserify](http://browserify.org/) and [reactify](https://github.com/andreypopp/reactify) for dependency management and JSX transformation.
+
+
+## Demo
+
+// TODO
+
+## Usage
+
+// TODO
+
+## License
+
+MIT
---
+++ src/ReactCreditCardInput.react.js
@@ -0,0 +1,96 @@
+/**
+ * @jsx React.DOM
+ */
+"use strict";
+
+var _ = require('lodash');
+var React = require('react');
+
+
+function cardFromNumber(num) {
+  num = (num + "").replace(/D/g, "");
+  for (var i = 0; i < cards.length; i++) {
+    var n = cards[i];
+    if (n.pattern.test(num))
+      return n;
+  }
+}
+
+var defaultFormat = /(\d{1,4})/g;
+
+var cards = [{
+                type: "visa",
+                pattern: /^4/,
+                format: defaultFormat,
+                length: [16],
+                cvcLength: [3],
+                luhn: true
+            }, {
+                type: "mastercard",
+                pattern: /^5[0-5]/,
+                format: defaultFormat,
+                length: [16],
+                cvcLength: [3],
+                luhn: true
+            }, {
+                type: "amex",
+                pattern: /^3[47]/,
+                format: /(\d{1,4})(\d{1,6})?(\d{1,5})?/,
+                length: [15],
+                cvcLength: [4],
+                luhn: true
+            }, {
+                type: "dinersclub",
+                pattern: /^3[0689]/,
+                format: defaultFormat,
+                length: [14],
+                cvcLength: [3],
+                luhn: true
+            }, {
+                type: "discover",
+                pattern: /^6([045]|22)/,
+                format: defaultFormat,
+                length: [16],
+                cvcLength: [3],
+                luhn: true
+            }, {
+                type: "jcb",
+                pattern: /^35/,
+                format: defaultFormat,
+                length: [16],
+                cvcLength: [3],
+                luhn: true
+            }];
+
+
+var ReactCreditCardInput = React.createClass({
+  handleCCNumberInput: function(e) {
+    var target = e.currentTarget,
+        n = target.value,
+        i = String.fromCharCode(e.which),
+        o = (n.replace(/\D/g, "") + i).length,
+        a = cardFromNumber(n + i),
+        s = 16;
+
+    if (a && (s = a.length), !/^\d+$/.test(i) || o > s)
+      return void e.preventDefault();
+
+    var l = a && "amex" === a.type ? /^(\d{4}|\d{4}\s\d{6})$/ : /(?:^|\s)(\d{4})$/;
+
+    return l.test(n) && target.selectionStart === n.length ? (e.preventDefault(), void(target.value = n + " " + i)) : void 0;
+  },
+
+  render: function() {
+    return (
+      <div className="react-credit-card-input">
+        <input
+          type="text"
+          autoComplete="off"
+          onKeyPress={this.handleCCNumberInput}
+          placeholder="• • • •   • • • •   • • • •   • • • •" />
+      </div>
+    );
+  }
+});
+
+module.exports = ReactCreditCardInput;
