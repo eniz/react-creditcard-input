7d3c4c9991027d6babbe20731c987afff0f671ca
Eniz Gülek <eniz.gulek@metglobal.com>
2015-05-20 11:57:45 +0300

    added test folder

---
+++ src/tests/CreditCardInput.js
@@ -0,0 +1,8 @@
+'use strict';
+
+import React from 'react/addons';
+
+const TestUtils = React.addons.TestUtils;
+const Simulate = TestUtils.Simulate;
+const SimulateNative = TestUtils.SimulateNative;
+
